7623baf81f7743012b8d919a0bfacbd76e745f7f
durgasai <venkatasai.947@gmail.com>
2022-01-13 22:45:47 -0500

    Update checkoutstep.groovy

--- vars/checkoutstep.groovy
+++ vars/checkoutstep.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String gitrep) {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: ${gitrep}]]])
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: "${gitrep}"]]])
 }
